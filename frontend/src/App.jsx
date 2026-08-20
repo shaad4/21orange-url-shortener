ff214f0d044cb6299d3e9cfbcd48ccf5d199fdda
shaad4 <mohammedshaadn@gmail.com>
2026-08-20 17:12:13 +0530

    fix: update to new version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -226,7 +226,7 @@ function App() {
               QR Gen: {health.qr}
             </span>
             <span className="version-badge" id="health-qr">
-              version: 1.2.1
+              version: 1.2.2
             </span>
           </div>
         </nav>
